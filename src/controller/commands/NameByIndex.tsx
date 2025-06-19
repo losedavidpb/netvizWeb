dd7f7e6cdbffcfae0cb3b5ec07c81f8b9e2faa48
losedavidpb <losedavidpb@gmail.com>
2025-06-19 20:34:47 +0100

    feat(controller): command to name vertices by the index

---
+++ src/controller/commands/NameByIndex.tsx
@@ -0,0 +1,43 @@
+import type { GLWindow } from "../../gui/GLWindow";
+import type { Command } from "../Command";
+
+/**
+ * NameByIndex :: Command to name the vertices using their index
+ *
+ * @author losedavidpb <losedavidpb@gmail.com>
+ */
+export class NameByIndex implements Command {
+
+    // --------------------------------
+    // Properties
+    // --------------------------------
+
+    private window: GLWindow;
+
+    /**
+     * Constructor for NameByIndex
+     *
+     * @param window parent window
+     */
+    constructor(window: GLWindow) {
+        this.window = window;
+    }
+
+    execute(): void {
+        const graph = this.window.getGraph();
+
+        if (graph !== undefined) {
+            const vertices = graph.getVertices();
+
+            for (let j = 0; j < vertices.length; ++j) {
+                vertices[j].setText(j + '');
+
+                if (this.window.getSelectedNode() === vertices[j]) {
+                    this.window.updateTextNode(vertices[j].getText());
+                }
+            }
+
+            this.window.refresh(false, false);
+        }
+    }
+}
