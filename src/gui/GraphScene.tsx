e5fa7d8264502210202d75bba7a6bb7c206ad077
losedavidpb <losedavidpb@gmail.com>
2025-06-30 20:18:30 +0100

    fix: remove camera adjustments

--- src/gui/GraphScene.tsx
+++ src/gui/GraphScene.tsx
@@ -1,8 +1,5 @@
 import { useFrame, useThree } from "@react-three/fiber";
-import { useEffect } from "react";
 import { OrbitControls } from "@react-three/drei";
-import * as THREE from 'three';
-
 import type { Graph } from "../model/Graph";
 
 /**
@@ -12,32 +9,7 @@ import type { Graph } from "../model/Graph";
  * @returns JSX element representing the graph
  */
 export const GraphScene = ({ graph }: { graph: Graph | null }) => {
-    const { camera, gl, size } = useThree();
-
-    useEffect(() => {
-        if (graph === null) return;
-
-        const pers_camera = camera as THREE.PerspectiveCamera;
-        const bounds = graph.getBoundingBox();
-
-        const center = bounds.getCenter(new THREE.Vector3());
-        const size = bounds.getSize(new THREE.Vector3());
-        const maxDim = Math.max(size.x, size.y, size.z);
-        const fov = pers_camera.fov * (Math.PI / 180);
-
-        const distance = maxDim / (2 * Math.tan(fov / 2));
-        const offset = 1.2;
-
-        const distancePosition = new THREE.Vector3(distance, distance, distance);
-        const newPosition = center.clone().add(distancePosition.multiplyScalar(offset));
-        pers_camera.position.copy(newPosition);
-
-        pers_camera.near = 0.1;
-        pers_camera.far = distance * 10;
-        pers_camera.updateProjectionMatrix();
-
-        pers_camera.lookAt(center);
-    }, [graph, camera]);
+    const { gl, size } = useThree();
 
     useFrame(() => {
         gl.setSize(size.width, size.height);
